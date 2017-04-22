e3a5fa26e6bcfad8a3c3df332ac88ff56f4e0c9c
Lucas Bamonte Corrêa <bamontelucas@gmail.com>
2017-04-21 21:11:10 -0300

    started session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,17 @@
 module.exports.index = function(application, req, res) {
-    res.render('index');
+    res.render('index', {validacao: []});
+}
+module.exports.autenticar = function(application, req, res) {
+    
+    req.assert('usuario', 'Campo usuário é obrigatório').notEmpty()
+    req.assert('senha', 'Campo senha é obrigatório').notEmpty()
+    
+    let erros = req.validationErrors();
+    if(erros) {
+        res.render('index', {validacao: erros});
+        return;
+    }
+
+    let UsuariosDAO = new application.app.models.UsuariosDAO(application.config.dbCOnnection);
+    UsuariosDAO.autenticar(req.body, req, res);
 }
--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -11,4 +11,26 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario) {
     });
 }
 
+UsuariosDAO.prototype.autenticar = function(usuario, req, res) {
+    this._connection.open(function(req, db) {
+        db.collection('usuarios', function(err, collection) {
+            collection.find(usuario).toArray(function(err, result){
+                if(result[0] !== undefined) {
+                    req.session.autorizado = true;
+                    req.session.usuario = result[0].usuario;
+                    req.session.casa = result[0].casa;
+                }
+
+                db.close();
+
+                if(req.session.autorizado) {
+                    res.redirect('jogo');
+                } else {
+                    res.render('index', {validacao: [{msg: 'Usuário ou senha incorretos'}]});
+                }
+            });
+        });
+    });
+}
+
 module.exports = () => UsuariosDAO;
